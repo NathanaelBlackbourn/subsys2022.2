c89acd78469550c3b49742b255914fd7d05eedd3
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2022-12-18 13:20:45 +0100

    Menu working via dataset attribute.

---
+++ src/ts/main.ts
@@ -0,0 +1,17 @@
+addEventListener('DOMContentLoaded', main)
+
+function main() {
+    addMenuListeners();
+}
+
+function addMenuListeners() {
+    const menuButtons: HTMLCollection = document.getElementsByClassName('main-nav-button');
+    for (const button of menuButtons) {
+        button.addEventListener('click', openSection);
+    }
+}
+
+function openSection(event) { // Ask David event type
+    const targetSection: HTMLElement | null = document.getElementById(event.target.dataset.opens);
+    targetSection?.classList.toggle('hidden');
+}
