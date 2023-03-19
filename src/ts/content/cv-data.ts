8a14dd53ffe0ee5fb536c5457b80ae6e381e61de
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-03-19 21:12:46 +0100

    Studying soon added to about. Menu positioned sticky.

--- src/ts/content/cv-data.ts
+++ src/ts/content/cv-data.ts
@@ -20,6 +20,7 @@ interface IntroBlock {
 interface SkillsBlocks {
   languagesSection: SkillsSection;
   skillsSection: SkillsSection;
+  studySoonSection: SkillsSection;
 }
 
 interface SkillsSection {
@@ -37,7 +38,7 @@ interface SkillColumn {
 
 interface Skill {
   skill: string;
-  level: number | string;
+  level?: number | string;
 }
 
 interface ExperienceBlocks {
@@ -74,7 +75,7 @@ const CVData: AboutContent = {
     languagesSection: {
       title: "I SPEAK",
       content: {
-        row1: {
+        col1: {
           lang1: {
             skill: "English",
             level: "native",
@@ -93,7 +94,7 @@ const CVData: AboutContent = {
     skillsSection: {
       title: "I WORK WITH",
       content: {
-        row1: {
+        col1: {
           skillBlock1: {
             skill: "HTML",
             level: 80,
@@ -107,7 +108,7 @@ const CVData: AboutContent = {
             level: 40,
           },
         },
-        row2: {
+        col2: {
           skillBlock1: {
             skill: "JavaScript",
             level: 60,
@@ -117,7 +118,7 @@ const CVData: AboutContent = {
             level: 40,
           },
         },
-        row3: {
+        col3: {
           skillBlock1: {
             skill: "React",
             level: 30,
@@ -127,7 +128,7 @@ const CVData: AboutContent = {
             level: 20,
           },
         },
-        row4: {
+        col4: {
           skillBlock1: {
             skill: "Adobe",
             level: 50,
@@ -135,6 +136,33 @@ const CVData: AboutContent = {
           skillBlock2: {
             skill: "Figma",
             level: 40,
+          },
+        },
+      },
+    },
+    studySoonSection: {
+      title: "SOON I WILL STUDY",
+      content: {
+        col1: {
+          skill1: {
+            skill: "Node",
+          },
+          skill2: {
+            skill: "Express",
+          },
+          skill3: {
+            skill: "REST-APIs",
+          },
+        },
+        col2: {
+          skill1: {
+            skill: "MongoDB",
+          },
+          skill2: {
+            skill: "Mongoose",
+          },
+          skill3: {
+            skill: "Socket.IO",
           },
         },
       },
